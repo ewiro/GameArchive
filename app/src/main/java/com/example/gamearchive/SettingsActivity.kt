b26725735a9587fda2c6097354c15ebb5ae6176e
ewiro <jihvvbjn@qq.com>
2026-07-14 20:12:27 +0800

    清理冗余

--- app/src/main/java/com/example/gamearchive/SettingsActivity.kt
+++ app/src/main/java/com/example/gamearchive/SettingsActivity.kt
@@ -33,7 +33,6 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
