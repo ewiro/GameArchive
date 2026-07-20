19226076f50519adb3ee9c421aeb70a672388d64
ewiro <jihvvbjn@qq.com>
2026-07-20 12:48:27 +0800

    v1.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.example.gamearchive"
         minSdk = 24
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.8"
+        versionCode = 10
+        versionName = "1.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
